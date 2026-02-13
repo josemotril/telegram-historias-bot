65ac4912f066523b57fac810f304f72bbb55f8b9
josemotril <josemotril@msn.com>
2026-02-13 09:07:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-# telegram-historias-bot
+import os
+import requests
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+mensaje = "Hola 👋 Tu bot de historias funciona correctamente"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": mensaje
+})
+
+print("Mensaje enviado")
